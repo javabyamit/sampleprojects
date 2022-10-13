b4125292a01e0de49c4715fb3ce4ff50d5d7a0ff
javabyamit <chauhanamit7506@gmail.com>
2022-10-12 18:24:20 -0900

    First commit

---
+++ Test.java
@@ -0,0 +1,20 @@
+class Super{
+	int i = 20;
+	
+}
+
+class sub extends Super{
+	int i = 30;
+	
+}
+
+
+public class inherit_exam_1 {
+	public static void main(String args[]) {
+		Super b = new sub();
+		System.out.println(b.i);
+		
+		
+		
+	}
+}
